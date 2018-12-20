3f362b97efedbe04f082c8ee46409e9ba673e0ca
ilham rofiqi <ilham.roiqi@gmai.com>
2018-12-20 21:54:48 +0700

    update project

--- app/src/main/java/com/example/ilhamrofiqi/uangkas/AddActivity.java
+++ app/src/main/java/com/example/ilhamrofiqi/uangkas/AddActivity.java
@@ -9,12 +9,15 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.andexert.library.RippleView;
+
 public class AddActivity extends AppCompatActivity {
 
     RadioGroup radio_status;
     RadioButton radio_masuk, radio_keluar;
     EditText edit_jumlah, edit_keterangan;
     Button btn_simpan;
+    RippleView rip_simpan;
 
     String status;
 
@@ -31,6 +34,7 @@ public class AddActivity extends AppCompatActivity {
         edit_jumlah         = findViewById(R.id.edit_jumlah);
         edit_keterangan     = findViewById(R.id.edit_keterangan);
         btn_simpan          = findViewById(R.id.btn_simpan);
+        rip_simpan          = findViewById(R.id.rip_simpan);
 
         radio_status.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -47,8 +51,30 @@ public class AddActivity extends AppCompatActivity {
         btn_simpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), " Jumlah : "+ edit_jumlah.getText().toString() + " Status : " + status,
-                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        rip_simpan.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+
+                if (status.equals("")){
+                    Toast.makeText(getApplicationContext(), "Status Harus Dilengkapi",
+                            Toast.LENGTH_LONG).show();
+                    radio_status.requestFocus();
+                } else if (edit_jumlah.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Jumlah Harus Diisi",
+                            Toast.LENGTH_LONG).show();
+                    edit_jumlah.requestFocus();
+                } else if (edit_keterangan.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Keterangan Harus Diisi",
+                            Toast.LENGTH_LONG).show();
+                    edit_keterangan.requestFocus();
+                } else {
+                    simpanData();
+                }
+
             }
         });
 
@@ -61,5 +87,10 @@ public class AddActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    private void simpanData(){
+        Toast.makeText(getApplicationContext(), "Data Keuangan Berhasil Disimpan",
+                Toast.LENGTH_LONG).show();
     }
 }
